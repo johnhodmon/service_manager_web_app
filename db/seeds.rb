50384855c1adac8501459194c7df3659b2d9b132
John Hodmon <johnhodmon@gmail.com>
2015-12-13 14:29:33 +0000

    seeded customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ c1=Customer.create(name:'Stella Byrne', 
+        email:'stellabyrne@jmail.com',
+       
+         street: "2 south street",
+         town: "New Ross",
+         county: "Wexford",
+         phone: "0519242233")
+
+   c2=Customer.create(name:'Roy Murphy', 
+        email:'roymurphy@jmail.com',
+       
+         street: "2 bride street",
+         town: "Wexford",
+         county: "Wexford",
+         phone: "0539124528")
+
+     c3=Customer.create(name:'Ciaran Meaney', 
+        email:'ciaranmeaney@jmail.com',
+       
+         street: "15 Patrick Street",
+         town: "Enniscorthy",
+         county: "Wexford",
+         phone: "0539255687")
+
+      c4=Customer.create(name:'Rie Nolan', 
+        email:'roynolan@jmail.com',
+       
+         street: "Ballyhack",
+         town: "New Ross",
+         county: "Wexford",
+         phone: "0513892305")
