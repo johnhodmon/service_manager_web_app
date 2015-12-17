c63b64e9a5155068529fe6269bf97a61cb94127c
John Hodmon <johnhodmon@gmail.com>
2015-12-17 14:03:09 +0000

    added route allowing subtype ManufacturedProduct be handled by Product#show

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
 
   delete 'logout' => 'sessions#destroy'
 
+  get 'manufactured_product'=>'products#show'
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
