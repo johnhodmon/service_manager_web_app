525cc5b41f02d8e9b9d3476c5e24d7e733cda98b
John Hodmon <johnhodmon@gmail.com>
2015-12-22 18:13:49 +0000

    fixed migration problem

--- db/migrate/20151217204441_add_job_id_to_invoices.rb
+++
@@ -1,5 +0,0 @@
-class AddJobIdToInvoices < ActiveRecord::Migration
-  def change
-    add_column :invoices, :job_id, :integer
-  end
-end
--- db/migrate/20151222151215_add_labour_time_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddLabourTimeToJobs < ActiveRecord::Migration
-  def change
-    add_column :jobs, :labour_time, :decimal
-  end
-end
