38e1399e9ba284a96b77526cbe525c6ecce0e559
John Hodmon <johnhodmon@gmail.com>
2015-12-23 18:06:20 +0000

    changed nav colors

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,8 +1,8 @@
 class Part < ActiveRecord::Base
 	has_many :used_on_jobs, :through => :job_parts, :source => :job
 	has_many :used_on_products, :through => :part_lists, :source => :product
-	has_many :part_lists
-	has_many :job_parts
+	has_many :part_lists, dependent: :destroy
+	has_many :job_parts, dependent: :destroy
 	validates :part_number, presence:true
 	validates :description, presence:true
 	validates :cost, presence:true
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
 	has_many :owned_by, :through => :customer_products, :source => :customer
 	has_many :customer_products, dependent: :destroy
 	has_many :part_lists, dependent: :destroy
+	
 	belongs_to :manufacturer
 	validates :product_number, presence:true
 	validates :description, presence:true
