6d223b92ccc9cce9f9933b3acd930aef2d516e4e
John Hodmon <johnhodmon@gmail.com>
2016-01-31 17:25:12 +0000

    modified routes to allow deleting of job parts from android

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
 get 'login' => 'sessions#new'
 post 'login'=> 'sessions#create'
 delete 'logout' =>'sessions#destroy'
+delete 'job_parts/:id'=>'job_parts#destroy'
 
 
 
