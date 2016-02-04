ba12b5b0aacb2d036c3926b720ca977fd2024d24
John Hodmon <johnhodmon@gmail.com>
2016-02-04 14:03:42 +0000

    modified json response to job to include list of job parts with description

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -4,10 +4,21 @@ module JobsHelper
 
 	part_list_with_part_numbers=part_list.map{
 		|pl| p=Part.find(pl.part_id)
-		{id:p.part_number,part_number:pl.part_id,description:p.description}
+		{part_id:p.id,part_number:p.part_number,description:p.description}
 	}
 
 	part_list_with_part_numbers
+
+end
+
+def getJobPartWithNums(job_parts)
+
+	job_part_with_part_numbers=job_parts.map{
+		|jp| p=Part.find(jp.part_id)
+		{jobPartId:jp.id,partNumber:p.part_number,description:p.description,quantity:jp.quantity}
+	}
+
+	job_part_with_part_numbers
 
 end
 
--- app/views/jobs/show.json.jbuilder
+++ app/views/jobs/show.json.jbuilder
@@ -6,7 +6,7 @@ json.( @job, :id, :reported_fault, :miles, :labour_time, :status)
   json.job_parts @job.job_parts
   json.part_lists @job.customer_product.product.part_lists
   json.part_lists_with_part_numbers getPartListWithNums(@job.customer_product.product.part_lists)
-
+  json.job_parts_with_part_numbers  getJobPartWithNums(@job.job_parts)
   
 
 
